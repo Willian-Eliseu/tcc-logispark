a6eaf284d6c91420b6d0fc65114c1c7205966f81
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-09-04 15:56:42 -0300

    login finalizado
    update 4/9 - 15h56

---
+++ js/login.js
@@ -0,0 +1,43 @@
+const formulary = document.getElementById('login-form');
+const url = "https://test.wl.tv.br/eliseu/tcc/api/login.php";
+
+formulary.onsubmit = (e) => {
+    e.preventDefault();
+    $('#submit-btn').prop('disabled', true);
+
+    $.post(url,
+        $('#login-form').serialize()
+    ).done(function(data){
+
+        if(data.user_active == "y"){
+            
+            sessionStorage.setItem('nome', data.user_name);
+            sessionStorage.setItem('email', data.user_email);
+            sessionStorage.setItem('key', data.user_key);
+            sessionStorage.setItem('nivel', data.user_level);
+            sessionStorage.setItem('active', data.user_active);
+
+            Swal.fire({
+                title: 'Bem vindo(a)!',
+                text: 'Bons estudos!!!',
+                icon: 'success',
+                showCancelButton: false,
+                confirmButtonColor: '#3085d6',
+                showConfirmButton: true,
+                confirmButtonText: 'OK'
+            }).then((result) => {
+                if (result.value) {
+                    window.location.href = "./ranking.html";
+                }
+            });
+        }else{
+            Swal.fire('Erro','Não foi possível realizar o login, por favor entre em contato conosco.','error');
+        }
+
+    }).fail(function(){
+        Swal.fire('Erro','Não foi possível realizar a operação desejada','error');
+    }).always(function(){
+        formulary.reset();
+        $('#submit-btn').prop('disabled', false);
+    });
+}
