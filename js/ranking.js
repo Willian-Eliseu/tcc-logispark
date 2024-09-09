9a0105dbc69b17722f4b8c9c9aa9e09256b8446f
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-09-09 16:05:49 -0300

    update ranking - 9/9/24 - 16h05

---
+++ js/ranking.js
@@ -0,0 +1,19 @@
+const url = "https://test.wl.tv.br/eliseu/tcc/api/ranking.php";
+
+$(function(){
+    $.get(url).done(function(data){
+        
+        $('#firstFase').html(data.fase[0].nome);
+        $('#secondFase').html(data.fase[1].nome);
+        $('#thirdFase').html(data.fase[2].nome);
+
+        $('#firstRandom').html(data.random[0].nome);
+        $('#secondRandom').html(data.random[1].nome);
+        $('#thirdRandom').html(data.random[2].nome);
+
+    }).fail(function(){
+        console.log("a página solicitada não está funcionando");
+    }).always(function(){
+        console.log("Tudo certo!!!");
+    });
+});
