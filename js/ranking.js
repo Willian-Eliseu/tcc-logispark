22eefdf205a60514a3a018abc37ae6b1173fba81
willian Eliseu da Silva <willian.eliseu@hotmail.com>
2024-10-13 20:22:57 -0300

    footer e ranking
    atualização no footer para exibir os conteúdos corretos.
    atualização no ranking. exibindo o botao de jogar apenas quando o usuário estiver logado

--- js/ranking.js
+++ js/ranking.js
@@ -11,6 +11,10 @@ $(function(){
         $('#secondRandom').html(data.random[1].nome);
         $('#thirdRandom').html(data.random[2].nome);
         */
+
+        if(sessionStorage.getItem('key')){
+            $('#jogaragora').removeClass('d-none');
+        }
     }).fail(function(){
         console.log("a página solicitada não está funcionando");
     }).always(function(){
