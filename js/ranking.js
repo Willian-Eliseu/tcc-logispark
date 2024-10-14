2be59d97a0f6f7637a6ebaaf55dd7ea659f74bf5
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-10-14 10:05:37 -0300

    Update ranking.js

--- js/ranking.js
+++ js/ranking.js
@@ -1,23 +1,26 @@
 const url = "https://test.wl.tv.br/eliseu/tcc/api/ranking.php";
 
-$(function(){
-    $.get(url).done(function(data){
-        
-        $('#firstFase').html(data.fase[0].nome);
-        $('#secondFase').html(data.fase[1].nome);
-        $('#thirdFase').html(data.fase[2].nome);
-        /*
+$(function () {
+  $.get(url)
+    .done(function (data) {
+      let resultado = JSON.parse(data);
+      $("#firstFase").html(resultado.fase[0].nome);
+      $("#secondFase").html(resultado.fase[1].nome);
+      $("#thirdFase").html(resultado.fase[2].nome);
+      /*
         $('#firstRandom').html(data.random[0].nome);
         $('#secondRandom').html(data.random[1].nome);
         $('#thirdRandom').html(data.random[2].nome);
-        */
+    */
 
-        if(sessionStorage.getItem('key')){
-            $('#jogaragora').removeClass('d-none');
-        }
-    }).fail(function(){
-        console.log("a página solicitada não está funcionando");
-    }).always(function(){
-        console.log("Tudo certo!!!");
+      if (sessionStorage.getItem("key")) {
+        $("#jogaragora").removeClass("d-none");
+      }
+    })
+    .fail(function () {
+      console.log("a página solicitada não está funcionando");
+    })
+    .always(function () {
+      console.log("Tudo certo!!!");
     });
 });
